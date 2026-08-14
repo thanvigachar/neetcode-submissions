3b9d2ca978d781020555a9168b0785b656be0218
thanvigachar <pes2ug23cs652@pesu.pes.edu>
2026-08-14 13:09:10 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s)!=len(t)):
+            return False
+        let1={}
+        let2={}
+        for i in range (len(s)):
+            let1[s[i]]=let1.get(s[i],0)+1
+            let2[t[i]]=let2.get(t[i],0)+1
+        if(let1==let2):
+            return True
+        return False
